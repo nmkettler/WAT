dff8e2e3655d24e92c242890784ef4092242ded6
Noah Kettler <nmkettler@gmail.com>
2016-10-04 05:10:02 -0700

    Initial Commit

---
+++ js/app.js
@@ -0,0 +1,28 @@
+var app = angular.module('watApp', ['ngRoute', 'firebase']);
+
+
+
+app.run(['$rootScope', '$location', function($rootScope, $location){
+    	$rootScope.$on('$routeChangeError', function(event, next, previous, error){ //traps an error if resolve map in /success isn't resolved properly
+    		if(error=='AUTH_REQUIRED'){
+    			$rootScope.message = 'Sorry you must login to view that page';
+    			$location.path('/login');
+    		}
+    	});
+	}]); //run
+
+app.config(['$routeProvider', function($routeProvider) {
+	  $routeProvider.
+	    when('/home', {
+	      templateUrl: 'views/home.html',
+	      controller: 'MainController'
+	    }).
+	    when('/test', {
+	      templateUrl: 'views/test.html',
+	      controller: 'MainController'
+	    }).
+	    otherwise({
+	      redirectTo: '/home'
+	    });
+
+}]);
---
+++ js/controllers/maincontroller.js
@@ -0,0 +1,4 @@
+app.controller("MainController", ['$scope', '$routeParams','$location', function($scope, $routeParams, $location) {
+	
+
+}]);
