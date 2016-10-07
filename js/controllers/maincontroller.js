b81ecddd45bb5a179c887f1c0132102c6b6af2af
Noah Kettler <nmkettler@gmail.com>
2016-10-07 14:54:17 -0700

    Completed Home

--- js/controllers/maincontroller.js
+++ js/controllers/maincontroller.js
@@ -1,4 +1,13 @@
 app.controller("MainController", ['$scope', '$routeParams','$location', function($scope, $routeParams, $location) {
+
+    $scope.date= new Date();
+  
+    //typed.js functionality
+        $(".typer").typed({
+            strings: ["Asbestos Testing", "Lead Testing", "Mold Testing", "Anything"],
+            typeSpeed: 100
+          });
+
 	$('ul.nav li.dropdown').hover(function(){
     $(this).find('.dropdown-menu').stop(true,true).delay(200).fadeIn(500);
   }, function(){
